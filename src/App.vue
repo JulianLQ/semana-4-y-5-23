aea73e4db9584a76feac2ed72760d7deaba6a9e3
JulianLQ <75047837+JulianLQ@users.noreply.github.com>
2020-12-23 17:17:54 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+
+    <br>
+
+    <section id="login" class="container-fluid">
+      <div class="container-fluid bg-light">
+        <the-login></the-login>
+      </div>
+    </section>
+    
+    <!-- <div id="team" class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <div class="col mt-5 h-100" v-for="(item, index) of team" :key="index">
+          <team-card :member="item"></team-card>
+        </div>
+      </div>
+    </div> -->
+
+    <br>
+        
+    <!-- <section id="news" class="noticias">
+      <div class="container-fluid bg-light">
+        <section-api></section-api>
+      </div>
+    </section> -->
+ 
+  </div>
+</template>
+
+<script>
+//import TeamCard from "./components/TeamCard.vue";
+import TheLogin from "./components/TheLogin.vue";
+//import SectionApi from "./components/SectionApi.vue";
+
+export default {
+  name: "App",
+  components: {   
+    //TeamCard, 
+    TheLogin,   
+    //SectionApi,            
+  },
+  
+  data() {
+    return {
+      team: [
+        {
+          codigo: 1,
+          nombre: "David Felipe Varón Molano",
+          descripcion:
+            "Estudiante de ingeniería mecánica de la Universidad Nacional de colombia, curioso por la programación, aficionado a los videojuegos y con ganas por mejorar la educación en colombia.",
+          rol: "Developement Team",
+          imagen: "/assets/davidvaron.jpg",
+        },
+        {
+          codigo: 2,
+          nombre: "Julián Andrés López Quintero",
+          descripcion: "Director de orquesta y compositor de musica para cine, TV y videojuegos; apasionado por el baile, aficionado a la programacion y a la cocina, dispuesto a aprender algo distinto cada dia y compartir el conocimiento.",
+          rol: "Developement Team",
+          imagen: "/assets/julianlopez.jpg",
+        },
+        {
+          codigo: 3,
+          nombre: "Rubén Darío Candia Parra",
+          descripcion: "Ingeniero Industrial y Especialista en gestión de proyectos, apasionado de la lectura, dibujo y el aprendizaje.",
+          rol: "Developement Team",
+          imagen: "/assets/rubencandia.jpg",
+        },
+        {
+          codigo: 4,
+          nombre: "Antonio Morales Correa",
+          descripcion: "Tecnólogo en Sistemas de Telecomunicaciones, apasionado de la lectura y videojuegos, en busca de nuevo conocimiento en programación y otras ramas de ella.",
+          rol: "Developement Team",
+          imagen: "/assets/antoniomorales.jpg",
+        },
+        {
+          codigo: 5,
+          nombre: "No Activo",
+          descripcion:
+            "Estudiante no activo.",
+          rol: "Developement Team",
+          imagen: "/assets/nn.webp",
+        },
+      ],
+    };
+  },
+};
+</script>
+
---
+++ src/components/TheLogin.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <form>
+
+      <div class="form-group">
+        <label for="exampleInputEmail1">Dirección electrónica</label>
+        <input
+          v-model="login.email"
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Ingresa tu e-mail"/>
+        <small id="emailHelp" class="form-text text-muted"
+          >Puedes estar tranquilo, no compartiremos tus datos.</small>
+      </div>
+
+      <div class="form-group">
+        <label for="exampleInputPassword1">Contraseña</label>
+        <input
+          v-model="login.password"
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          placeholder="Contraseña"/>
+      </div>
+
+      <!-- <pre>
+        {{ login }}
+      </pre> -->
+
+      <!-- <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div> -->
+
+      <button type="submit" class="btn btn-primary">Ingresar</button>
+
+    </form>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "TheLogin",
+    data(){
+      return{
+        login:{
+          email: '',
+          password: ''
+        }
+      }
+    }
+    
+  };
+</script>
+
+<style scoped>
+
+</style>
